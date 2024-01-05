75bc3806110efd64f6791df6fe745228ae847f73
Nitesh Pandey <pandeynitesh1302@gmail.com>
2024-01-05 21:13:26 +0530

    added login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,42 @@
 import React from "react";
+import Header from "./Header";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="bg"
+        />
+      </div>
+
+      <form
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white opacity-80"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email address "
+          className="p-4 my-2 w-full bg-gray-700 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-2 w-full bg-gray-700 rounded-lg"
+        />
+        <button className="p-4 my-6 w-full bg-red-700 rounded-lg">
+          Sign Up
+        </button>
+        <p className="p-4">New to Movies GPT? Sign Up Now</p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
