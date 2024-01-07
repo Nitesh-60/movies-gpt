ce83186976ff1d4683296acc636442008aceb030
Nitesh Pandey <pandeynitesh1302@gmail.com>
2024-01-07 10:28:55 +0530

    completed authentication && redux store setup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,11 @@
 import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate";
+import { auth } from "../utils/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const Login = () => {
   const [isSigninPage, setIsSigninPage] = useState(true);
@@ -21,7 +26,41 @@ const Login = () => {
       password?.current?.value
     );
     setErrorMessage(message);
-    // SignIn / SignUp
+
+    if (message) return;
+
+    if (!isSigninPage) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + " - " + errorMessage);
+        });
+    } else {
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + " - " + errorMessage);
+        });
+    }
   };
 
   return (
